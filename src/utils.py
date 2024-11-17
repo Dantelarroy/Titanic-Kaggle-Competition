3a9c4e478ec13c406a84a792a335bce1b24d9bbe
Dante <dalarroy@emeal.nttdata.com>
2024-11-17 19:20:41 -0300

    initial EDA 17.11

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,21 @@
 # src/utils.py
 '''En este archivo se agregaran:
 Funciones para cargar y preprocesar los datos'''
+
+# src/utils.py
+import pandas as pd
+
+def columnas_a_minusculas(df):
+    """Convierte los nombres de todas las columnas a minúsculas."""
+    df.columns = [col.lower() for col in df.columns]
+    return df
+
+def eliminar_columna(df: pd.DataFrame, columna: str) -> pd.DataFrame:
+    """Elimina una columna de un DataFrame."""
+    if columna in df.columns:
+        df = df.drop(columns=[columna])
+        print(f"Columna '{columna}' eliminada exitosamente.")
+    else:
+        print(f"Error: La columna '{columna}' no existe en el DataFrame.")
+    return df
+

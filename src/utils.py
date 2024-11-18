0e60d14cd62b0b6b0b1d5e5fe92a35059ec4070a
Dante <dalarroy@emeal.nttdata.com>
2024-11-18 10:45:22 -0300

    Cambios realizados

--- src/utils.py
+++ src/utils.py
@@ -5,17 +5,17 @@ Funciones para cargar y preprocesar los datos'''
 # src/utils.py
 import pandas as pd
 
+def eliminar_columna(df: pd.DataFrame, columna: str):
+        """Elimina una columna de un DataFrame."""
+        if columna in df.columns:
+            df = df.drop(columns=[columna])
+            print(f"Columna '{columna}' eliminada exitosamente.")
+        else:
+            print(f"Error: La columna '{columna}' no existe en el DataFrame.")
+        return df
+
+
 def columnas_a_minusculas(df):
     """Convierte los nombres de todas las columnas a minúsculas."""
     df.columns = [col.lower() for col in df.columns]
     return df
-
-def eliminar_columna(df: pd.DataFrame, columna: str) -> pd.DataFrame:
-    """Elimina una columna de un DataFrame."""
-    if columna in df.columns:
-        df = df.drop(columns=[columna])
-        print(f"Columna '{columna}' eliminada exitosamente.")
-    else:
-        print(f"Error: La columna '{columna}' no existe en el DataFrame.")
-    return df
-
